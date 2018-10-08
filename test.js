fb0dcbede88e4eb619e2ca95bcdd3bf6a9705880
Steelhawk <steel_hawk@list.ru>
2018-10-08 08:04:55 +0500

    First commit test

---
+++ test.js
@@ -0,0 +1,8 @@
+var n = prompt ('Введите число', '');
+console.log('Console Output - realy?');
+function sumTo(n) {
+	for (var i = 1; i < n; i++) {
+		var sum = i + 1;
+	}
+	return alert( sumTo(n) );
+}	
